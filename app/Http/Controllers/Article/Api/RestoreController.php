a0c606d43f76a96317554b479e6a0cf1dc97e195
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-05-31 18:04:57 +0200

    user-role console test suit upd, api store-, update- and restore-controllers upd, article api service error response upd, 1 test added to article api test suit

--- app/Http/Controllers/Article/Api/RestoreController.php
+++ app/Http/Controllers/Article/Api/RestoreController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers\Article\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Article\ArticleResource;
 use App\Http\Services\Article\Api\ArticleApiService;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\JsonResponse;
 
 final class RestoreController extends Controller
 {
-    public function __invoke(string $id): ArticleResource
+    public function __invoke(string $id): ArticleResource|JsonResponse
     {
-        $restoredArticle = ArticleApiService::restore($id);
+        $result = ArticleApiService::restore($id);
 
-        return new ArticleResource($restoredArticle);
+        return $result instanceof Model ? new ArticleResource($result) : response()->json($result, 422);
     }
 }
